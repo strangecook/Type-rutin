6b3ced3348a0c60832ac44a2aec42723a1ef8d13
Applehole <skyheo123@naver.com>
2022-07-13 21:28:08 +0900

    Feat: add Dummy Data

--- my-app/src/components/OnlyToday.tsx
+++ my-app/src/components/OnlyToday.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components'
 import { TodayComponent, TodayTitle, TodayTodo, TodayTodoInput, TodayTodoNumber, TodayTodoText, TodayTodoFix, TodayTodoDel } from '../Styled/TodayStyled';
+import { TodayData } from '../dummy/todayData';
+import { useState } from 'react';
 
 function OnlyToday() {
   return (
@@ -8,13 +9,24 @@ function OnlyToday() {
             <TodayTitle>
               오늘 할 일 리스트
             </TodayTitle>
-            <TodayTodo>
+            {
+              TodayData.map((el,idx)=>{
+                return <TodayTodo>
+                  <TodayTodoInput type="checkbox"></TodayTodoInput>
+                  <TodayTodoNumber>{idx+1}.</TodayTodoNumber>
+                  <TodayTodoText>{el.data}</TodayTodoText>
+                  <TodayTodoFix>수정</TodayTodoFix>
+                  <TodayTodoDel>삭제</TodayTodoDel>
+                </TodayTodo>
+              })
+            }
+            {/* <TodayTodo>
               <TodayTodoInput type="checkbox"></TodayTodoInput>
               <TodayTodoNumber>1.</TodayTodoNumber>
               <TodayTodoText>이게 매일 할일 1이다</TodayTodoText>
               <TodayTodoFix>수정</TodayTodoFix>
               <TodayTodoDel>삭제</TodayTodoDel>
-            </TodayTodo>
+            </TodayTodo> */}
         </TodayComponent>
   );
 }
---
+++ my-app/src/dummy/todayData.tsx
@@ -0,0 +1,5 @@
+export const TodayData = [
+    { data : "heap, stack, queue 만들기",
+      change : false
+}
+]
