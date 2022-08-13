d7aeb9d42f3aa003469fe5f3afe1c147a88eaf9b
Applehole <skyheo123@naver.com>
2022-08-13 22:15:15 +0900

    Feat: connect wirh login Firebase

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -5,15 +5,8 @@ import Rutin from '../components/Home/rutin';
 import OnlyToday from '../components/Home/OnlyToday';
 import Calender from '../components/Home/Calender'
 import Approach from '../components/Home/Approach';
-import { Unsubscribe } from "@firebase/auth";// @으로 임포트 해오면 해당 타입들이 적혀져 있다.
 
-interface homeProps {
-  isLoggedIn : boolean,
-  userObj : Unsubscribe | {}
-}
-
-
-function Home({isLoggedIn, userObj}: homeProps) {
+function Home() {
   return (
     <HomeContainer>
       <InputSection />
