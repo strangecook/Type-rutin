85463f027bfa7c4ab9065c0bf55f4176c0a2c841
Applehole <skyheo123@naver.com>
2022-08-29 23:51:45 +0900

    Fix: D3.js Frame Fix

--- my-app/src/components/Home/Approach.tsx
+++ my-app/src/components/Home/Approach.tsx
@@ -6,32 +6,31 @@ import * as d3 from "d3";
 function Approach() {
   const [data, setData] = useState(["50%", 250, 70])
   useEffect(() => {
-
     d3.selectAll(".data")
-      .attr("width", 10)
+      .attr("width", 0)
+      .attr("height", 30)
       .data(data)
       .transition().duration(1000)
-      .attr("height", 40)
+      .attr("height", 30)
       .attr("fill", "green")
       .attr("width", (d) => d)
-
   }, [data])
   return (
     <ApproachContainer>
       <ChartDiv>
         <div className='todayApproach'>오늘 할당량</div>
         <svg className='todayApproachGraph' width="100%" height="auto">
-          <rect rx="10" width="100%" height="40" y="0" fill="#B3C680" ></rect>
+          <rect rx="10" width="100%" height="30" y="0" fill="#B3C680" ></rect>
           <rect className='data' rx="10" ></rect>
         </svg>
         <div className='weekApproach'>일주일 할당량</div>
         <svg className='weekApproachGraph' width="100%" height="auto">
-          <rect rx="10" width="100%" height="40" y="0" fill="#B3C680" ></rect>
+          <rect rx="10" width="100%" height="30" y="0" fill="#B3C680" ></rect>
           <rect className='data' rx="10" ></rect>
         </svg>
         <div className='everyApproach'>전체 할당량</div>
         <svg className='everyApproachGraph' width="100%" height="auto">
-          <rect rx="10" width="100%" height="40" y="0" fill="#B3C680" ></rect>
+          <rect rx="10" width="100%" height="30" y="0" fill="#B3C680" ></rect>
           <rect className='data' rx="10" ></rect>
         </svg>
       </ChartDiv>
