ba4bbad0c4be737bd6ec2c515bac4c09ea2aed3b
Applehole <skyheo123@naver.com>
2022-09-09 17:57:45 +0900

    Feat: Font-awesome Add

--- my-app/src/components/Navigation/Navigation.tsx
+++ my-app/src/components/Navigation/Navigation.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom"
 import { NaviCoverDiv } from "../../Styled/Navigation/navigationStyle"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
     return (
         <NaviCoverDiv>
-            <ul>
+            <FontAwesomeIcon icon={faBars} size="3x" />
+            {/* <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/profile">my profile</Link></li>
-            </ul>
-        </NaviCoverDiv>
+            </ul> */}
+        </NaviCoverDiv> 
     );
 }
 
