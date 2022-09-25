5045eedfd3b11af4faafbc16aaf761164af95a0d
Applehole <skyheo123@naver.com>
2022-09-25 22:37:55 +0900

    Feat: user LeftTasks

---
+++ my-app/src/Styled/LeftWorkStyled/TotalPercentageStyled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const TotalPercentageContainer = styled.div`
+grid-area: total;
+border-radius: 10px;
+overflow: hidden;
+`
+
+export const TotalPercentageChartDiv = styled.div`
+    height: 230px;
+    width: 90%;
+    margin: auto;
+`
