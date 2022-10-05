d9a85fbd61fe1e39f57de50ffc5453c14a6fa600
Applehole <skyheo123@naver.com>
2022-10-05 22:32:13 +0900

    Refector: leftTask css refector

--- my-app/src/Pages/LeftWorks.tsx
+++ my-app/src/Pages/LeftWorks.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { LeftWorksComponent } from '../Styled/LeftWorkStyled/LeftWorkStyled';
-import Navigation from '../components/Navigation/Navigation';
-import SideBar from '../components/Home/SideBar';
-import TotalPercentage from '../components/LeftWorks/TotalPercentage';
-import LeftTasks from '../components/LeftWorks/LeftTasks';
+import React from "react";
+import { LeftWorksComponent } from "../Styled/LeftWorkStyled/LeftWorkStyled";
+import Navigation from "../components/Navigation/Navigation";
+import SideBar from "../components/Home/SideBar";
+import TotalPercentage from "../components/LeftWorks/TotalPercentage";
+import LeftTasks from "../components/LeftWorks/LeftTasks";
 
 function LeftWorks() {
   return (
