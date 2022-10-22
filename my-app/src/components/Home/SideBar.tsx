fca60b2a849e6d2260956c01f67b6fda093e2883
Applehole <skyheo123@naver.com>
2022-10-22 21:57:22 +0900

    time

--- my-app/src/components/Home/SideBar.tsx
+++ my-app/src/components/Home/SideBar.tsx
@@ -1,39 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { SideBarComponent } from '../../Styled/HomeStyle/SideBarStyled';
-import SidebarStore from "../../store/sidebarStore"
+import React, { useState, useEffect } from "react";
+import { SideBarComponent } from "../../Styled/HomeStyle/SideBarStyled";
+import SidebarStore from "../../store/sidebarStore";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faCalendarCheck, faSadCry } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom'
+import {
+  faHome,
+  faCalendarCheck,
+  faSadCry,
+} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 function SideBar() {
   const { sideState } = SidebarStore();
   let navigate = useNavigate();
 
-  const goToHomePage = () =>{
-    navigate("/")
-  }
-
-  const goToLeftWorksPage = () =>{
-    navigate("/left")
-  }
-  const goToDoneWorksPage = () =>{
-    navigate("/done")
-  }
+  const goToHomePage = () => {
+    navigate("/");
+  };
+  const goToLeftWorksPage = () => {
+    navigate("/left");
+  };
+  const goToDoneWorksPage = () => {
+    navigate("/done");
+  };
 
   return (
     <SideBarComponent sideState={sideState}>
       <div className="sidebarcover">
         <div onClick={goToHomePage} className="homeSide">
-          <FontAwesomeIcon className='homeIcon' icon={faHome} size="3x"></FontAwesomeIcon>
-          <div className='homeIconName' >홈페이지</div>
+          <FontAwesomeIcon
+            className="homeIcon"
+            icon={faHome}
+            size="3x"
+          ></FontAwesomeIcon>
+          <div className="homeIconName">홈페이지</div>
         </div>
         <div onClick={goToLeftWorksPage} className="untodoList">
-          <FontAwesomeIcon className='todolistIcon' icon={faSadCry} size="3x"></FontAwesomeIcon>
-          <div className='todolistIconName'>밀린 과제</div>
+          <FontAwesomeIcon
+            className="todolistIcon"
+            icon={faSadCry}
+            size="3x"
+          ></FontAwesomeIcon>
+          <div className="todolistIconName">밀린 과제</div>
         </div>
         <div onClick={goToDoneWorksPage} className="untodoList">
-          <FontAwesomeIcon className='todolistIcon' icon={faCalendarCheck} size="3x"></FontAwesomeIcon>
-          <div className='todolistIconName'>해결된 과제</div>
+          <FontAwesomeIcon
+            className="todolistIcon"
+            icon={faCalendarCheck}
+            size="3x"
+          ></FontAwesomeIcon>
+          <div className="todolistIconName">해결된 과제</div>
         </div>
       </div>
     </SideBarComponent>
--- my-app/src/components/Navigation/Navigation.tsx
+++ my-app/src/components/Navigation/Navigation.tsx
@@ -1,30 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import { NaviCoverDiv } from "../../Styled/Navigation/navigationStyle"
+import { Link } from "react-router-dom";
+import { NaviCoverDiv } from "../../Styled/Navigation/navigationStyle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars,faX } from "@fortawesome/free-solid-svg-icons";
-import SidebarStore from "../../store/sidebarStore"
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import SidebarStore from "../../store/sidebarStore";
 
 function Navigation() {
-    const { sideState, increasePopulation } = SidebarStore();
+  const { sideState, increasePopulation } = SidebarStore();
 
-    const SideChange = () =>{
-        increasePopulation()
-    }
-    return (
-        <NaviCoverDiv>
-            <div className="SideBar">
-                {sideState 
-                ? <FontAwesomeIcon onClick={()=>SideChange()} className="SideBarIcon" icon={faX} size="3x" /> 
-                : <FontAwesomeIcon onClick={()=>SideChange()} className="SideBarIcon" icon={faBars} size="3x" />
-                }
-            </div>
-            {/* <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/profile">my profile</Link></li>
-            </ul> */}
-        </NaviCoverDiv> 
-    );
+  const SideChange = () => {
+    increasePopulation();
+  };
+  return (
+    <NaviCoverDiv>
+      <div className="SideBar">
+        {sideState ? (
+          <FontAwesomeIcon
+            onClick={() => SideChange()}
+            className="SideBarIcon"
+            icon={faX}
+            size="3x"
+          />
+        ) : (
+          <FontAwesomeIcon
+            onClick={() => SideChange()}
+            className="SideBarIcon"
+            icon={faBars}
+            size="3x"
+          />
+        )}
+      </div>
+    </NaviCoverDiv>
+  );
 }
 
 export default Navigation;
